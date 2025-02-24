21098ecba363a831498c0f5a97d6e90c12b4050f
j1yuu <o.lover4@bk.ru>
2025-02-24 13:36:19 +0300

    #0.0.1: fixed endpoints (http -> https)

--- src/entities/endpoints.ts
+++ src/entities/endpoints.ts
@@ -1,2 +1,2 @@
-export const GEOCODING_ENDPOINT = "http://api.openweathermap.org/geo/1.0/direct?q="
-export const FORECAST_ENDPOINT = "http://api.openweathermap.org/data/2.5/forecast?"
+export const GEOCODING_ENDPOINT = "https://api.openweathermap.org/geo/1.0/direct?q="
+export const FORECAST_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast?"
